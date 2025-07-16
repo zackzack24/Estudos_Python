4419ea63d9fe8168a0a2dcbee17bf6cec1e4f973
Isaac <luzisaac24@gmail.com>
2025-07-16 20:27:49 -0300

    Aula 04 Matrizes e Coleções

---
+++ Aula 04/colecoes.py
@@ -0,0 +1,20 @@
+conjunto = set([4, 7, 2, 3, 0, 8])
+print("Conjunto:", conjunto)
+
+tupla = (3, 2, 4, 0, 1, 5)
+print(tupla)
+
+pessoas1 = ['Isaac', 'João', 'Maria']
+print(pessoas1)
+
+pessoa = {'nome': 'Isaac', 'telefone': '12 9 3456 7891' , 'cidade': 'São Paulo'}
+print(pessoa['nome'])
+
+pessoas = [ 
+    {'nome': 'Isaac', 'telefone': '12 9 3456 7891', 'cidade': 'São Paulo'},
+    {'nome': 'João', 'telefone': '12 9 3456 7892', 'cidade': 'Rio de Janeiro'},
+    {'nome': 'Maria', 'telefone': '12 9 3456 7893', 'cidade': 'Belo Horizonte'},
+    {'nome': 'Ana', 'telefone': '12 9 3456 7894', 'cidade': 'Curitiba'}
+]
+
+print(pessoas[0]['nome'])
---
+++ Aula 04/matrizes.py
@@ -0,0 +1,11 @@
+numeros = [
+        [20, 36, 75], 
+        [12, 45, 90], 
+        [91, 92, 92], 
+        [33, 88, 82],
+]
+
+for i in numeros:
+    print(i)
+
+print(numeros[2][1])
